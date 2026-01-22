f13c364d0c5fbb683d62d16a0dc66872a14e2de6
Nadira Seitkazy <nadiraseitkazu@gmail.com>
2026-01-22 10:17:35 +0500

    Initial Commit

---
+++ ProfileManagement/ProfileManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileManagementApp.swift
+//  ProfileManagement
+//
+//  Created by Nadira Seitkazy  on 22.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct ProfileManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
